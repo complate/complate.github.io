1c036a1637d3fcba578ba44b6ea7b2fad71cc770
Joy Clark <joy.clark@innoq.com>
2020-02-12 16:11:28 +0100

    Added a Breadcrumb component and Icon components

---
+++ views/components/breadcrumb/index.jsx
@@ -0,0 +1,25 @@
+import { createElement } from "complate-stream";
+
+import Icon from "../icons/index"
+
+export function BreadcrumbLink ({ href }, ...children) {
+  return <a href={href}>
+    {children}
+    <Icon symbol="icon-arrow-right" />
+  </a>;
+}
+
+/* This is an example of a component where we could use DOM
+modification to add the `aria-current` Attribute to the last
+`a` child in the breadcrumb. */
+export function CurrentPage ({ href }, ...children) {
+  return <a href={href} aria-current="page">
+    {children}
+  </a>;
+}
+
+export default (_params, ...children) => <nav class="breadcrumb">
+  <ol>
+    {children.map(child => <li>{child}</li>)}
+  </ol>
+</nav>;
