0fa73b24076b00e1fa705986a4fd00df6c0850ba
Joy Clark <joy.clark@innoq.com>
2020-02-12 21:48:36 +0100

    Add meta tag for mobile phones

--- views/layout.jsx
+++ views/layout.jsx
@@ -4,6 +4,7 @@ export default function DefaultLayout({ title }, ...children) {
 	return <html>
 		<head>
 			<meta charset="utf-8" />
+			<meta content="width=device-width, initial-scale=1.0" name="viewport" />
 			<title>{title}</title>
 			<link href="/assets/fonts.css" rel="stylesheet" />
 			<link href="/assets/index.css" rel="stylesheet" />
