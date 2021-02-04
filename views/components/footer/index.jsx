61ae49854dcc8fc51fbd51e4c76da645d0ac34a9
Marc Jansing <marc.jansing@innoq.com>
2021-02-04 11:32:51 +0100

    fix imprint link

--- views/components/footer/index.jsx
+++ views/components/footer/index.jsx
@@ -2,7 +2,7 @@ import { createElement } from "complate-stream";
 
 export default function Footer() {
 	return <footer>
-		<a href="https://www.innoq.com/en/imprint/">imprint</a>
+		<a href="https://innoq.com/en/impressum/">imprint</a>
 		<a href="https://innoq.com/datenschutz">privacy</a>
 	</footer>
 }
