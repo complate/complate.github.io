2a4171d9ca5e549cd3fe4d69a8a11c18fdbdc2ff
FND <fnd@localhost.localdomain>
2023-02-03 14:55:42 +0100

    fixed privacy link, ensuring correct locale

--- views/components/footer/index.jsx
+++ views/components/footer/index.jsx
@@ -3,6 +3,6 @@ import { createElement } from "complate-stream";
 export default function Footer() {
 	return <footer>
 		<a href="https://innoq.com/en/impressum/">imprint</a>
-		<a href="https://innoq.com/datenschutz">privacy</a>
+		<a href="https://innoq.com/en/datenschutz">privacy</a>
 	</footer>
 }
